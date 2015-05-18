0333c0feb06d23dfa1fc663f432167f1b3fc1b96
Alexandre Collet <alexandre.collet@supinfo.com>
2015-05-18 20:06:09 +0200

    Search results page : use array in controller to generate videos previews

--- app/controllers/search.controller.js
+++ app/controllers/search.controller.js
@@ -9,4 +9,79 @@ function SearchController($routeParams){
 
 	vm.string = $routeParams.string;
 
+	vm.videos = [
+		{
+			name : "Shared #A",
+			img  : "http://lorempicsum.com/futurama/400/225/1"
+		},
+		{
+			name : "Shared #B",
+			img  : "http://lorempicsum.com/futurama/400/225/2"
+		},
+		{
+			name : "Shared #A",
+			img  : "http://lorempicsum.com/futurama/400/225/1"
+		},
+		{
+			name : "Shared #B",
+			img  : "http://lorempicsum.com/futurama/400/225/2"
+		},
+		{
+			name : "Shared #A",
+			img  : "http://lorempicsum.com/futurama/400/225/1"
+		},
+		{
+			name : "Shared #B",
+			img  : "http://lorempicsum.com/futurama/400/225/2"
+		},
+		{
+			name : "Shared #A",
+			img  : "http://lorempicsum.com/futurama/400/225/1"
+		},
+		{
+			name : "Shared #B",
+			img  : "http://lorempicsum.com/futurama/400/225/2"
+		},
+		{
+			name : "Shared #A",
+			img  : "http://lorempicsum.com/futurama/400/225/1"
+		},
+		{
+			name : "Shared #B",
+			img  : "http://lorempicsum.com/futurama/400/225/2"
+		},
+		{
+			name : "Shared #A",
+			img  : "http://lorempicsum.com/futurama/400/225/1"
+		},
+		{
+			name : "Shared #B",
+			img  : "http://lorempicsum.com/futurama/400/225/2"
+		},
+		{
+			name : "Shared #A",
+			img  : "http://lorempicsum.com/futurama/400/225/1"
+		},
+		{
+			name : "Shared #B",
+			img  : "http://lorempicsum.com/futurama/400/225/2"
+		},
+		{
+			name : "Shared #C",
+			img  : "http://lorempicsum.com/futurama/400/225/3"
+		},
+		{
+			name : "Shared #D",
+			img  : "http://lorempicsum.com/futurama/400/225/4"
+		},
+		{
+			name : "Shared #E",
+			img  : "http://lorempicsum.com/futurama/400/225/5"
+		},
+		{
+			name : "Shared #F",
+			img  : "http://lorempicsum.com/futurama/400/225/6"
+		}
+	];
+
 }
