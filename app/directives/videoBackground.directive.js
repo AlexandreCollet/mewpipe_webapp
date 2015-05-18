844f010727191522ca8351a59344081832316631
Alexandre Collet <alexandre.collet@supinfo.com>
2015-05-18 16:13:18 +0200

    Create videoBackground directive

---
+++ app/directives/videoBackground.directive.js
@@ -0,0 +1,76 @@
+angular.module('mewpipe')
+	   .directive('videoBackground', videoBackground);
+
+function videoBackground(){
+
+	/**
+	 * DEFAULT VARIABLES
+	 */
+	
+	var RATIO = 16/9;
+
+	var videoBackgroundOptions = {
+		controls : false,
+		autoplay : true,
+		loop     : true,
+		muted    : true,
+	};
+
+	/**
+	 * DIRECTIVE
+	 */
+	
+	var directive = {
+		templateUrl : '/app/directives/videoBackground.directive.html',
+		restrict    : 'EA',
+		link        : link
+	}
+
+	return directive;
+
+	/**
+	 * FUNCTIONS
+	 */
+
+	function link(scope, element, attrs){
+
+		var videoElement = element.context.getElementsByTagName('video')[0];
+
+		var player = videojs(videoElement, videoBackgroundOptions);
+		
+		player.muted(true);
+		player.src([
+			{ type: 'video/mp4', src: '/assets/videos/'+attrs.name+'.mp4' }
+		]);
+
+		_setVideoSizes(player);
+
+		window.addEventListener('resize', function(){ _setVideoSizes(player); });
+
+		element.bind("$destroy", function(){ player.dispose(); });
+	}
+
+	function _setVideoSizes(video){
+
+		var windowWidth  = window.innerWidth;
+		var windowHeight = window.innerHeight;
+
+		var sizes = {
+			width  : windowWidth,
+			height : windowWidth/RATIO
+		}
+
+		if(sizes.height < windowHeight){
+			sizes = {
+				height : windowHeight,
+				width  : windowHeight*RATIO
+			}
+		}
+
+		video.width(sizes.width);
+		video.height(sizes.height);
+		
+		video.el().style.left = -(sizes.width-windowWidth)/2 + "px";
+	}
+
+}
--- assets/js/index.js
+++ assets/js/index.js
@@ -4,19 +4,7 @@
 	 * DEFAULT VARIABLES
 	 */
 	
-	var RATIO  = 16/9;
-
 	var searchInput = document.getElementById('search_input');
-
-	var videosMostViewedContainer = document.getElementById('mostViewed');
-	var videosMostSharedContainer = document.getElementById('mostShared');
-
-	var videoBackgroundOptions = {
-		controls : false,
-		autoplay : true,
-		loop     : true,
-		muted    : true,
-	}
 
 	/**
 	 * MAIN
@@ -25,18 +13,6 @@
 
 	searchFocus();
 
-	var videoBackground = videojs("video_background", videoBackgroundOptions);
-	videoBackground.muted(true); //patch muted default options for firefox 
-	setVideoSizes(videoBackground);
-
-
-	/**
-	 * EVENTS
-	 */
-	
-	window.addEventListener('resize',function(event){
-		setVideoSizes(videoBackground);
-	});
 
 	/**
 	 * FUNCTIONS
@@ -46,28 +22,6 @@
 		searchInput.focus();
 	}
 
-	function setVideoSizes(video){
-
-		var windowWidth  = window.innerWidth;
-		var windowHeight = window.innerHeight;
-
-		var sizes = {
-			width  : windowWidth,
-			height : windowWidth/RATIO
-		}
-
-		if(sizes.height < windowHeight){
-			sizes = {
-				height : windowHeight,
-				width  : windowHeight*RATIO
-			}
-		}
-
-		video.width(sizes.width);
-		video.height(sizes.height);
-		
-		video.el().style.left = -(sizes.width-windowWidth)/2 + "px";
-	}
 
 
 })();
