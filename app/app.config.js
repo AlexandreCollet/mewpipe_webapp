965fdbab19d8af35a8e54e076b1d1071cb8ec055
Alexandre Collet <alexandre.collet@supinfo.com>
2015-05-21 16:20:52 +0200

    Get mails sending by share form

--- app/app.config.js
+++ app/app.config.js
@@ -9,5 +9,8 @@ angular.module('mewpipe')
 	   		},
 	   		requests : {
 	   			defaultLimit : 10
+	   		},
+	   		regex : {
+	   			email : new RegExp('^[0-9a-z._-]+@{1}[0-9a-z.-]{2,}[.]{1}[a-z]{2,5}$','i')
 	   		}
 	   });
--- app/controllers/share.controller.js
+++ app/controllers/share.controller.js
@@ -1,14 +1,46 @@
 angular.module('mewpipe')
 	   .controller('ShareController', ShareController);
 
-ShareController.$inject = ['$scope'];
+ShareController.$inject = ['$scope','Config'];
 
-function ShareController($scope){
+function ShareController($scope,Config){
 
-	$scope.shareIt = shareIt
+	$scope.regexEmail = Config.regex.email;
+	$scope.emails     = new Array(1);
+
+	$scope.shareIt = shareIt;
+	$scope.onChange  = onChange;
 
 	function shareIt(){
-		console.log("coucou");
+		
+		var emailsLength = $scope.emails.length;
+		var validEmails  = [];
+
+		for(var i=0;i<emailsLength;i++){
+			if($scope.regexEmail.test($scope.emails[i])){
+				validEmails.push($scope.emails[i]);
+			}
+		}
+
+		console.log(validEmails);
+	}
+
+	function onChange(index){
+
+		var emailsLength = $scope.emails.length;
+		if(index !== emailsLength - 1) return;
+
+
+		var addInput = true;
+		for(var i=0;i<emailsLength;i++){
+			if($scope.emails[i] === ""){
+				addInput = false;
+			}
+		}
+
+		if(addInput){
+			$scope.emails.push(undefined);
+		}
 	}
 
 }
