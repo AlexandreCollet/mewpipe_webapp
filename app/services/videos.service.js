c01334338c1e8cffc181559daa17a16a45733365
Alexandre Collet <alexandre.collet@supinfo.com>
2015-06-01 19:32:52 +0200

    Beautify videos service

--- app/services/videos.service.js
+++ app/services/videos.service.js
@@ -6,30 +6,69 @@ video.$inject = ['$resource','Config'];
 
 function video($resource,Config) {
 	return $resource( Config.server.url + ':' + Config.server.port + '/api/videos/:id', { id : '@id'}, {
-		update : { method : 'PUT' },
-		remove : { method : 'DELETE'},
-		search  : { method : 'GET', params: {
-			s      : "",
-			limit  : Config.requests.defaultLimit, 
-			offset : 0 
-		} },
-		findOne : { method : 'GET' },
-		findMostShared : { method : 'GET', params: {
-			limit: 6,
-			ordering : '-weekly_share'
-		}},
-		findMostPlayed : { method : 'GET', params: {
-			limit: 6,
-			ordering: '-weekly_view'
-		}},
-		findByUser : { method : 'GET', params: {
-			offset : 0,
-			limit  : Config.requests.defaultLimit,
-			ordering : '-creation_date'
-		}},
+
+		/**
+		 * GET
+		 */
+
+		findOne : { 
+			method : 'GET' 
+		},
+
+		findMostShared : {
+			method : 'GET', 
+			params: {
+				limit: 6,
+				ordering : '-weekly_share'
+			}
+		},
+
+		findMostPlayed : { 
+			method : 'GET', 
+			params: {
+				limit: 6,
+				ordering: '-weekly_view'
+			}
+		},
+
+		findByUser : { 
+			method : 'GET', 
+			params: {
+				offset : 0,
+				limit  : Config.requests.defaultLimit,
+				ordering : '-creation_date'
+			}
+		},
+
+		search  : { 
+			method : 'GET', 
+			params: {
+				s      : "",
+				limit  : Config.requests.defaultLimit, 
+				offset : 0 
+			} 
+		},
+
+		/**
+		 * PUT, PATCH, POST, DELETE
+		 */
+
+		update : { 
+			method : 'PUT'    
+		},
+
+		remove : { 
+			method : 'DELETE' 
+		},
+
+		/**
+		 * OTHER ACTIONS
+		 */
+		
 		share : {
 			method : 'POST',
 			url    : Config.server.url + ':' + Config.server.port + '/api/videos/:id/share'
 		}
+
 	} );
 }
