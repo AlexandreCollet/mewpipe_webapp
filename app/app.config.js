6082cf1b150920fb62f9647ad1858d6b0be8ac05
Alexandre Collet <alexandre.collet@supinfo.com>
2015-06-02 23:13:46 +0200

    Import and configure satellizer

--- app/app.config.js
+++ app/app.config.js
@@ -1,11 +1,35 @@
 angular.module('mewpipe')
-	   .config(function(toastrConfig,$resourceProvider){
+	   .config(function(toastrConfig,$resourceProvider,$authProvider,Config){
+
+	   		/**
+	   		 * Toastr
+	   		 */
 
 	   		angular.extend(toastrConfig, {
 	   			positionClass : 'toast-bottom-right',
 	   			closeButton   : true,
 	   		});
 
+	   		/**
+	   		 * Resource Provider
+	   		 */
+
 	   		$resourceProvider.defaults.stripTrailingSlashes = false;
+
+	   		/**
+	   		 * Auth Provider
+	   		 */
+	   		$authProvider.loginOnSignup = false;
+	   		$authProvider.baseUrl  = Config.server.url + ':' + Config.server.port + '/api';
+	   		$authProvider.loginUrl = '/login';
+	   		$authProvider.signupUrl = '/registration/';
+	   		$authProvider.loginRoute = '/login';
+	   		$authProvider.signupRoute = '/register';
+	   		$authProvider.unlinkUrl = '/logout';
+	   		$authProvider.unlinkMethod = 'post';
+
+	   		$authProvider.facebook({
+		      	clientId: '950692341649325'
+		    });
 
 	   });
