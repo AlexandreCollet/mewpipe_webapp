4e32e65efff7057b9d3496da5eeb60e0d200fbe2
Alexandre Collet <alexandre.collet@supinfo.com>
2015-06-04 21:51:31 +0200

    Middleware authentication

--- app/app.run.js
+++ app/app.run.js
@@ -6,7 +6,18 @@ run.$inject = ['$rootScope','$location','$auth'];
 function run($rootScope, $location, $auth){
 
 	$rootScope.$on('$routeChangeStart', function(event, next){
-		console.log(next.data.authentication);
+
+		var acceptAnonymous = next.data.authentication.anonymous;
+		var acceptConnected = next.data.authentication.connected;
+		
+		if(acceptAnonymous && acceptConnected) return;
+
+		var isAuthenticated = $auth.isAuthenticated();
+
+		if( isAuthenticated  && acceptConnected || !isAuthenticated && acceptAnonymous) return;
+
+		//$location.path('/login');
+
 	});
 
 }
