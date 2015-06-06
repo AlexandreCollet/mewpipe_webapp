ed5b3c0eda0416aa138624c1c25f0e31edb3e900
Alexandre Collet <alexandre.collet@supinfo.com>
2015-06-06 18:10:03 +0200

    Create users service with update and change password requests

---
+++ app/services/users.service.js
@@ -0,0 +1,28 @@
+angular
+	.module('mewpipe')
+	.factory('usersService', usersService);
+
+usersService.$inject = ['$resource','Config'];
+
+function usersService($resource,Config) {
+	return $resource( Config.server.url + ':' + Config.server.port + '/api/user', {
+
+		/**
+		 * PUT, PATCH, POST, DELETE
+		 */
+
+		update : { 
+			method : 'PUT'    
+		},
+		
+		/**
+		 * OTHER ACTIONS
+		 */
+		
+		changePassword: {
+			method : 'POST',
+			url    : Config.server.url + ':' + Config.server.port + '/api/user/password'
+		}
+
+	} );
+}
