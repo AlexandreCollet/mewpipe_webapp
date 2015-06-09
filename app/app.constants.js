4b7adf56ebc8baa0fdbc6a8fa08e1e1b835238aa
Alexandre Collet <alexandre.collet@supinfo.com>
2015-06-09 22:25:26 +0200

    Change video requests default limit

--- app/app.constants.js
+++ app/app.constants.js
@@ -6,7 +6,7 @@ angular.module('mewpipe')
 		   		port : 8000,
 	   		},
 	   		requests : {
-	   			defaultLimit : 10
+	   			defaultLimit : 25
 	   		},
 	   		regex : {
 	   			email    : new RegExp('^[0-9a-z._-]+@{1}[0-9a-z.-]{2,}[.]{1}[a-z]{2,5}$','i'),
